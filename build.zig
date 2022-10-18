60c96083e8cb06b438ffeb326760158199de5bb2
zed <dee0xeed@gmail.com>
2022-10-18 18:19:40 +0300

    modified:   build.zig

--- build.zig
+++ build.zig
@@ -4,11 +4,10 @@ pub fn build(b: *std.build.Builder) void {
 
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("edsm-09", "src/main.zig");
+    const exe = b.addExecutable("xjiss", "src/main.zig");
     exe.linkLibC();
     exe.linkSystemLibrary("X11");
     exe.linkSystemLibrary("asound");
-//    exe.linkSystemLibrary("libpulse");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
