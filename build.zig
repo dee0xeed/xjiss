b0b8b77275cfd384dbee9ab8c9c28e593e125504
zed <dee0xeed@gmail.com>
2022-10-22 12:28:40 +0300

    modified:   build.zig

--- build.zig
+++ build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("xjiss", "src/main.zig");
+    exe.single_threaded = true;
     exe.linkLibC();
     exe.linkSystemLibrary("X11");
     exe.linkSystemLibrary("asound");
